b174b44c6cd3d21494d1ed5fa4589de02525ed8d
Riley <tay0008@jmss.vic.edu.au>
2016-09-05 15:04:25 +1000

    Learning about hidden data, figuring out what it means

--- parsematlab_rats.py
+++ parsematlab_rats.py
@@ -2,14 +2,25 @@ import scipy.io as sio
 
 all_659602 = sio.loadmat('659602_rec03_all.mat')
 
-print(all_659602['__header__'])
+#print(all_659602['__header__'])
 print(all_659602.keys())
-print()
-sch_wave = all_659602['Sch_wav'][0][0][4]
-for i in sch_wave:
+print(all_659602['__header__'])
+
+wave_timestamp = all_659602['Sch_wav'][0][0][4]
+stim_timestamp = all_659602['StimTrig'][0][0][4]
+stim_amplitude = all_659602['StimTrig'][0][0][5]
+
+print(len(wave_timestamp))
+print(len(stim_timestamp))
+print(len(stim_amplitude))
+"""
+for i in range(len(stim_timestamp)):
+    print(stim_timestamp[i], stim_amplitude[i])
+"""
+for i in wave_timestamp:
     print(i)
-    #pass
-stimtrig = all_659602['StimTrig'][0][0][4]
-for i in stimtrig:
-    #print(i)
-    pass
+
+#print(sch_wave)
+
+#print(sch_wave[0], sch_wave[-1])
+#print(stimtrig[0], stimtrig[-1])
