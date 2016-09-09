131b346f8a5cba9b23ab406e3669e99a8db13e87
Riley <tay0008@jmss.vic.edu.au>
2016-09-09 12:50:27 +1000

    Trying to sort out amplitudes, etc

--- parsematlab_rats.py
+++ parsematlab_rats.py
@@ -2,7 +2,6 @@ import scipy.io as sio
 
 file = sio.loadmat('659605_rec03_all.mat')
 
-#print(file['__header__'])
 print(file['__header__'])
 print(file.keys())
 
@@ -10,26 +9,32 @@ wave_timestamp = file['Sch_wav'][0][0][4]
 stim_timestamp = file['StimTrig'][0][0][4]
 stim_amplitude = file['StimTrig'][0][0][5]
 
-stufflist = []
-
-#print(len(wave_timestamp))
-#print(len(stim_timestamp))
-#print(len(stim_amplitude))
+sortedvals = []
 
 for i in range(len(stim_timestamp)):
-    stufflist += [[float("%.6f" % stim_timestamp[i][0]), stim_amplitude[i][0]]]
+    sortedvals += [[float("%.6f" % stim_timestamp[i][0]), stim_amplitude[i][0]]]
 
-for i in range(len(stufflist)):
-    stime = stufflist[i][0]
+for i in range(len(sortedvals)):
+    stime = sortedvals[i][0]
     pops = []
     for j in wave_timestamp:
         if j>=stime:
             if j<=float(stime)+0.05:
                 pops += [float("%.3f" % ((j[0]-float(stime))*1000))]
-                #print((j[0]-float(stime))*1000)
             else:
                 break
-    stufflist[i] += [pops]
+    sortedvals[i] += [pops]
 
-for i in stufflist:
+for i in sortedvals:
     print(i)
+"""
+experiment_group = [[]]*10
+print(experiment_group)
+for i in sortedvals:
+    if i[1] == 62:
+        break
+    else:
+        experiment_group[i[1]-1] = i
+
+print(experiment_group)
+"""
