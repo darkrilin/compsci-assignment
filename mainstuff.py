e39951fee3d6205305f6ee6524a3dd7f5d248bb0
Riley <tay0008@jmss.vic.edu.au>
2016-09-13 11:43:13 +1000

    Added subsorting of experiment groups within matlab loading function.

--- mainstuff.py
+++ mainstuff.py
@@ -1,12 +1,5 @@
 import parsematlab_rats
 sortedvals = parsematlab_rats.extractmatlab('659605_rec03_all.mat')
 
-experiment_group = [[]]*10
 for i in sortedvals:
-    if i[1] == 62:
-        break
-    else:
-        experiment_group[i[1]-1] = i
-
-for i in experiment_group:
     print(i)
--- parsematlab_rats.py
+++ parsematlab_rats.py
@@ -8,13 +8,14 @@ def extractmatlab(filename):
     wave_timestamp = file['Sch_wav'][0][0][4]   # Timestamps of neurons firing
     stim_timestamp = file['StimTrig'][0][0][4]  # Timestamps of stimulus
     stim_amplitude = file['StimTrig'][0][0][5]  # Relative amplitude of stimulus
+    randomvals = []
     sortedvals = []
 
     for i in range(len(stim_timestamp)): # Concatenating stimulus timestamp with amplitude
-        sortedvals += [[float("%.6f" % stim_timestamp[i][0]), stim_amplitude[i][0]]]
+        randomvals += [[float("%.6f" % stim_timestamp[i][0]), stim_amplitude[i][0]]]
 
-    for i in range(len(sortedvals)): # Calculate neuron fires within 50ms of trigger, add to list
-        stime = sortedvals[i][0]
+    for i in range(len(randomvals)): # Calculate neuron fires within 50ms of trigger, add to list
+        stime = randomvals[i][0]
         pops = []
         for j in wave_timestamp:
             if j>=stime:
@@ -22,7 +23,14 @@ def extractmatlab(filename):
                     pops += [float("%.3f" % ((j[0]-float(stime))*1000))]
                 else:
                     break
-        sortedvals[i] += [pops]
+        randomvals[i] += [pops]
+
+    experimentbatch = [[]]*10
+    for i in range(len(randomvals)):
+        if randomvals[i][1] == 62:
+            sortedvals += experimentbatch + [62]
+        else:
+            experimentbatch[randomvals[i][1]-1] = randomvals[i]
 
     # FORMAT OF ITEMS IN SORTEDVALS:
     # [stim timestamp, stim amplitude, [list of neurons firing within 50ms of stim]]
