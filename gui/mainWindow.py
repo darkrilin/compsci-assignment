bb4713ac711c6db1cfa37762691474b2d6a7dba4
WilliamCStark <williamstarkemail@gmail.com>
2016-09-25 22:39:55 +1000

    Added further menu items with shortcuts and events

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -5,15 +5,14 @@ import sys
 class MainWindow(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, title="Neuroscience Graph Maker", pos=(150,150), size=(1200,700))
-
+        # Initialise the UI
         self.InitUI()
 
     def InitUI(self):
         # Create the status bar
         self.CreateStatusBar()
-
+        # Initialise the menu bar
         self.InitMenuBar()
-
         # Top level frame stuff
         self.panel = wx.Panel(self)
         self.Centre()
@@ -39,15 +38,15 @@ class MainWindow(wx.Frame):
 
         self.FileMenu.AppendSeparator()
 
-        self.SettingsItem = wx.MenuItem(self.FileMenu, wx.ID_ANY, text="Settings", help="Edit Application Settings")
+        self.SettingsItem = wx.MenuItem(self.FileMenu, wx.ID_ANY, text="&Settings", help="Edit Application Settings")
         self.FileMenu.AppendItem(self.SettingsItem)
 
         self.FileMenu.AppendSeparator()
 
-        self.ImportItem = wx.MenuItem(self.FileMenu, wx.ID_ANY, text="Import", help="Import MATLAB data")
+        self.ImportItem = wx.MenuItem(self.FileMenu, wx.ID_ANY, text="&Import", help="Import MATLAB data")
         self.FileMenu.AppendItem(self.ImportItem)
 
-        self.ExportAsItem = wx.MenuItem(self.FileMenu, wx.ID_ANY, text="Export as...", help="Open the export dialog to export data")
+        self.ExportAsItem = wx.MenuItem(self.FileMenu, wx.ID_ANY, text="&Export as...", help="Open the export dialog to export data")
         self.FileMenu.AppendItem(self.ExportAsItem)
 
         self.FileMenu.AppendSeparator()
@@ -57,10 +56,11 @@ class MainWindow(wx.Frame):
 
         self.FileMenu.AppendSeparator()
 
-        self.ExitItem = wx.MenuItem(self.FileMenu, wx.ID_EXIT, text="Exit")
+        self.ExitItem = wx.MenuItem(self.FileMenu, wx.ID_EXIT, text="&Exit\tCtrl+Q")
+        self.SetAcceleratorTable(wx.AcceleratorTable([(wx.ACCEL_CTRL, ord('Q'), self.ExitItem.GetId())]))
         self.FileMenu.AppendItem(self.ExitItem)
 
-        self.MenuBar.Append(self.FileMenu, "File")
+        self.MenuBar.Append(self.FileMenu, "&File")
 
         # Edit Menu
         self.EditMenu = wx.Menu()
@@ -68,13 +68,100 @@ class MainWindow(wx.Frame):
         self.UndoItem = wx.MenuItem(self.EditMenu, wx.ID_UNDO)
         self.EditMenu.AppendItem(self.UndoItem)
 
-        self.MenuBar.Append(self.EditMenu, "Edit")
+        self.RedoItem = wx.MenuItem(self.EditMenu, wx.ID_ANY, text='&Redo\tCtrl+Y', help="Redo last action")
+        self.SetAcceleratorTable(wx.AcceleratorTable([(wx.ACCEL_CTRL, ord('Y'), self.RedoItem.GetId())]))
+        self.EditMenu.AppendItem(self.RedoItem)
 
+        self.EditMenu.AppendSeparator()
+
+        self.CutItem = wx.MenuItem(self.EditMenu, wx.ID_CUT)
+        self.EditMenu.AppendItem(self.CutItem)
+
+        self.CopyItem = wx.MenuItem(self.EditMenu, wx.ID_COPY)
+        self.EditMenu.AppendItem(self.CopyItem)
+
+        self.PasteItem = wx.MenuItem(self.EditMenu, wx.ID_PASTE)
+        self.EditMenu.AppendItem(self.PasteItem)
+
+        self.DeleteItem = wx.MenuItem(self.EditMenu, wx.ID_DELETE)
+        self.EditMenu.AppendItem(self.DeleteItem)
+
+        self.MenuBar.Append(self.EditMenu, "&Edit")
+
+        # View Menu
+        self.ViewMenu = wx.Menu()
+
+        self.ZoomInItem = wx.MenuItem(self.ViewMenu, wx.ID_ZOOM_IN)
+        self.ViewMenu.AppendItem(self.ZoomInItem)
+
+        self.ZoomOutItem = wx.MenuItem(self.ViewMenu, wx.ID_ZOOM_OUT)
+        self.ViewMenu.AppendItem(self.ZoomOutItem)
+
+        self.MenuBar.Append(self.ViewMenu, "&View")
+
+        # Set the menu bar
         self.SetMenuBar(self.MenuBar)
-        self.Bind(wx.EVT_MENU, self.OnQuit, self.ExitItem)
+        # Bind events to menu items
+        self.Bind(wx.EVT_MENU, self.OnNew, self.NewItem)
+        self.Bind(wx.EVT_MENU, self.OnOpen, self.OpenItem)
+        self.Bind(wx.EVT_MENU, self.OnSave, self.SaveItem)
+        self.Bind(wx.EVT_MENU, self.OnSaveAs, self.SaveAsItem)
+        self.Bind(wx.EVT_MENU, self.OnSettings, self.SettingsItem)
+        self.Bind(wx.EVT_MENU, self.OnImport, self.ImportItem)
+        self.Bind(wx.EVT_MENU, self.OnExportAs, self.ExportAsItem)
+        self.Bind(wx.EVT_MENU, self.OnPrint, self.PrintItem)
+        self.Bind(wx.EVT_MENU, self.OnExit, self.ExitItem)
+        self.Bind(wx.EVT_MENU, self.OnUndo, self.UndoItem)
+        self.Bind(wx.EVT_MENU, self.OnRedo, self.RedoItem)
+        self.Bind(wx.EVT_MENU, self.OnCut, self.CutItem)
+        self.Bind(wx.EVT_MENU, self.OnCopy, self.CopyItem)
+        self.Bind(wx.EVT_MENU, self.OnPaste, self.PasteItem)
+        self.Bind(wx.EVT_MENU, self.OnDelete, self.DeleteItem)
 
-    def OnQuit(self, e):
+    def OnNew(self, e):
+        print("New")
+
+    def OnOpen(self, e):
+        print("Open")
+
+    def OnSave(self, e):
+        print("Save")
+
+    def OnSaveAs(self, e):
+        print("SaveAs")
+
+    def OnSettings(self, e):
+        print("Settings")
+
+    def OnImport(self, e):
+        print("Import")
+
+    def OnExportAs(self, e):
+        print("ExportAs")
+
+    def OnPrint(self, e):
+        print("Print")
+
+    def OnExit(self, e):
         self.Close()
+
+    def OnUndo(self, e):
+        print("Undo")
+
+    def OnRedo(self, e):
+        print("Redo")
+
+    def OnCut(self, e):
+        print("Cut")
+
+    def OnCopy(self, e):
+        print("Copy")
+
+    def OnPaste(self, e):
+        print("Paste")
+
+    def OnDelete(self, e):
+        print("Delete")
 
 class MyApp(wx.App):
     def OnInit(self):
