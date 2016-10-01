f3657d729273cfa7282baaa1e06faf9fa0898e22
Riley <tay0008@jmss.vic.edu.au>
2016-10-01 11:54:34 +1000

    First test plots

--- mainstuff.py
+++ mainstuff.py
@@ -1,5 +1,34 @@
 import parsematlab_rats
-sortedvals = parsematlab_rats.extractmatlab('659601_rec03_all.mat')
+import plotly.plotly as py
+import plotly.graph_objs as go
+sortedvals = parsematlab_rats.extractmatlab('659605_rec03_all.mat')
+py.sign_in('tay0008', '9f03ec53cm')
 
+cheesarinoes = []
+n = []
 for i in sortedvals:
-    print(i)
+    if i == 62:
+        cheesarinoes += [n]
+        n = []
+    else:
+        n += [i]
+
+coordinates = []
+scale = len(cheesarinoes)
+
+for i in range(scale):
+    for j in cheesarinoes[i]:
+        y = j[1]+i/scale
+        for n in j[2]:
+            x = n
+            coordinates += [(x,y)]
+
+trace = go.Scatter(
+    x = [i[0] for i in coordinates],
+    y = [i[1] for i in coordinates],
+    mode = 'markers'
+)
+
+data = [trace]
+
+py.iplot(data, filename='test-scatter_02')
