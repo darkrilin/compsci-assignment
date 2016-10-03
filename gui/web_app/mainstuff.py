ff0633d154cd06c5f7249116f502abb63156516d
Riley <tay0008@jmss.vic.edu.au>
2016-10-03 19:40:40 +1100

    Stopped auto-opening of generated html files
    Also fixed bug with os import

--- gui/web_app/mainstuff.py
+++ gui/web_app/mainstuff.py
@@ -1,7 +1,7 @@
 import parsematlab_rats
 import plotly.plotly as py
 import plotly.graph_objs as go
-import os.path, os.getcwd, os.makedirs
+import os
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
 def ensure_dir(f):
@@ -45,16 +45,11 @@ def plot_with_plotly(filename):
         print(filename + " graphed successfully")
 
     for i in trace:
-        plot([trace[i]], filename=i+'.html')
+        plot([trace[i]], filename=i+'.html', auto_open=False)
 
 
 # TEMPORARY TESTING CODE; REMOVE IN FINAL BUILD
 if __name__ == '__main__':
-    allfiles = ['659601_rec03_all.mat', '659602_rec03_all.mat',
-                '659603_rec03_all.mat', '659604_rec03_all.mat',
-                '659605_rec03_all.mat', '659606_rec03_all.mat',
-                '659607_rec03_all.mat', '659608_rec03_all.mat',
-                '659609_rec03_all.mat', '659610_rec03_all.mat']
     files = ['659601_rec03_all.mat']
     for i in files:
         plot_with_plotly(i)
