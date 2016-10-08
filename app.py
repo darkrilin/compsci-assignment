ed22491cd2645bf63c95adbd0047612dcc9edd09
Riley <tay0008@jmss.vic.edu.au>
2016-10-08 15:39:37 +1100

    Fixed syntax error (I should have tested this before pushing)

--- app.py
+++ app.py
@@ -65,9 +65,7 @@ def graph_file(filename):
 
 HEROKU = os.environ.get('HEROKU', 0)
 if HEROKU:
-    print("WHOOOOSH")
     PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT)
-else
-    print('failure')
+else:
     app.run(host='localhost')
