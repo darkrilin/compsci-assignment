a77d188c04bf5bc6901960dced90719d64b3adb6
Riley <tay0008@jmss.vic.edu.au>
2016-10-08 14:42:03 +1100

    Fixed bad static location

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import main
 import os
 
 UPLOAD_FOLDER = 'uploads/'
-STATIC_FOLDER = 'static/'
-ALLOWED_EXTENSIONS = set(['mat'])
+ALLOWED_EXTENSIONS = {'mat'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.static_folder = os.getcwd()+STATIC_FOLDER
 
 # I just want to say Will, I am so very sorry for anything
 # I may have done to your precious files. Hopefully you can still
 # test this thing locally, because it sort of works on heroku now.
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 def ensure_dir(f):
     d = os.path.dirname(os.getcwd() + f)
@@ -27,6 +27,7 @@ def ensure_dir(f):
 @app.route('/')
 def show_main():
     return render_template('website.html')
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -48,21 +49,23 @@ def upload_file():
             return redirect('/graph/' + filename + '.html')
         return redirect("/")
 
+
 @app.route('/graph/<filename>')
 def graph_file(filename):
     try:
         file = open(app.config['UPLOAD_FOLDER'] + filename)
-    except:
+    except Exception as ex:
+        print(ex)
         return redirect('/')
     file_html = file.read()
     file.close()
-    #os.remove(app.config['UPLOAD_FOLDER'] + filename)
+    # os.remove(app.config['UPLOAD_FOLDER'] + filename)
     return file_html
 
 
-PORT = int(os.environ.get('PORT', 5000))
 HEROKU = os.environ.get('HEROKU', 0)
 if HEROKU:
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT)
 else:
     app.run(host='localhost')
