72594e7513241c88be62fe80fe83d84bc641a02d
Riley Taylor <darkrilin@gmail.com>
2018-01-27 19:37:33 +1100

    Fixed example path

--- app.py
+++ app.py
@@ -95,6 +95,11 @@ def graph_file(filename):
     return file_html
 
 
+@app.route('/example/')
+def show_example():
+    return render_template('example.html')
+
+
 def insert_substring(main_string, substring, index):
     return main_string[:index] + substring + main_string[index:]
 
