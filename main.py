7d9bf73cfbdf8082768ce0cad8b2026c1e85b611
Riley Taylor <darkrilin@gmail.com>
2018-01-27 18:24:09 +1100

    Layers can now be toggled
    Also:
    -Added support for matplotlib colourmaps
    -Added support for widgets and JS
    -Removed the old extraction code to clean up (it's still in older
    versions)

--- main.py
+++ main.py
@@ -3,46 +3,18 @@ import scipy.io as sio
 
 from bokeh.plotting import figure, show, output_file, save, ColumnDataSource
 from bokeh.models import HoverTool, CrosshairTool, PanTool, WheelZoomTool, UndoTool, \
-    RedoTool, ResetTool, SaveTool, PolySelectTool
+    RedoTool, ResetTool, SaveTool, PolySelectTool, CustomJS
+
+from bokeh.models.widgets import CheckboxGroup, Button
+from bokeh.layouts import widgetbox, layout, row, column
+
+import matplotlib as plt
+import matplotlib.cm as cm
 
 import numpy as np
 
 
 # Different ways of extracting the matlab files - or their varying structure standards
-def extract_old(filename):
-    print("WARNING: YOU ARE USING THE OLD EXTRACTION CODE, WHICH IS REALLY SLOW AND BUGGY")
-    file = sio.loadmat(filename)
-
-    wave_timestamp = file['Sch_wav'][0][0][4]  # Neuron pop - milliseconds since trigger
-    stim_timestamp = file['StimTrig'][0][0][4]  # Stimulus time into experiment - seconds
-    stim_amplitude = file['StimTrig'][0][0][5]  # Amplitude of particular stimulus time
-
-    randomvals = []
-    sortedvals = []
-
-    for i in range(len(stim_timestamp)):
-        randomvals += [[float("%.6f" % stim_timestamp[i][0]), stim_amplitude[i][0]]]
-
-    for i in range(len(randomvals)):
-        stime = randomvals[i][0]
-        pops = []
-        for j in wave_timestamp:
-            if j >= stime:
-                if j <= float(stime)+0.05:
-                    pops += [float("%.3f" % ((j[0]-float(stime))*1000))]
-                else:
-                    break
-        randomvals[i] += [pops]
-
-    experimentbatch = [[]]*10
-    for i in range(len(randomvals)):
-        if randomvals[i][1] == 62:
-            sortedvals += experimentbatch + [62]
-        else:
-            experimentbatch[randomvals[i][1]-1] = randomvals[i]
-
-    return sortedvals
-
 def extract_matlab_all(filename):
     ### --- STRUCTURE: xxxxxx_rec03_all.mat --- ###
     file = sio.loadmat(filename)
@@ -196,23 +168,26 @@ def bokeh_composite(filename, auto_open=True, colour="black", w=500, h=250, radi
 
 
         # GENERATE PLOT WITH CUSTOM PROPERTIES
-        TOOLS = [Hover, CrosshairTool(), PanTool(), WheelZoomTool(), UndoTool(), RedoTool(), ResetTool(), SaveTool(), PolySelectTool()]
+        TOOLS = [Hover, CrosshairTool(), PanTool(), WheelZoomTool(), ResetTool(), SaveTool()]
         p = figure(tools=TOOLS, title="Composite Plot: " + file[file.find('/') + 1::], plot_width=50, plot_height=10)
 
         p.sizing_mode = "stretch_both"
         p.border_fill_color = "whitesmoke"
         p.min_border_left = 80
         p.min_border_right = 80
-        p.min_border_bottom = 80
+        p.min_border_bottom = 50
+        p.min_border_top = 20
         p.xaxis.axis_label = "Time (ms)"
         p.yaxis.axis_label = "Amplitude"
+        p.width = 160
+        p.height = 70
 
 
         # ADD HEATMAP
         raw = np.zeros((h, w))
         top = 0
 
-        # TODO: CLEAN UP CIRCLE CODE
+        # TODO: CLEAN UP CIRCLE CODE, OPTIMISE THE SHIT OUT OF IT
         for pos in coordinates:
             x_pos = floor((pos[1]-1)/10*h)
             y_pos = floor(pos[0]/50*w)
@@ -232,11 +207,16 @@ def bokeh_composite(filename, auto_open=True, colour="black", w=500, h=250, radi
                 raw[i, j] = (raw[i, j] / top)
                 raw[i, j] = 1 - raw[i, j] #Invert data for colourmap
 
-        p.image(image=[raw], x=0, y=0, dw=50, dh=10, palette="Inferno256")#"YlOrBr9")#
+        # Fetch matplotlib colourmaps, convert to bokeh
+        colormap = cm.get_cmap("RdPu")
+        bokehpalette = [plt.colors.rgb2hex(m) for m in colormap(np.arange(colormap.N))][::-1]
+
+        # Render image
+        image = p.image(image=[raw], x=0, y=0, dw=50, dh=10, palette=bokehpalette)#"Inferno256")#"YlOrBr9")
 
 
         # ADD SCATTER MAP
-        p.scatter('x', 'y', radius=0.1, fill_alpha=0.8, line_color=None, color=colour, source=source, name="dots")
+        scatter = p.scatter('x', 'y', radius=0.1, fill_alpha=0.8, line_color=None, color=colour, source=source, name="dots")
 
 
         # ADD AMPLITUDE LINES
@@ -244,15 +224,37 @@ def bokeh_composite(filename, auto_open=True, colour="black", w=500, h=250, radi
             p.line((0, 50), (i, i), color=colour, alpha=0.5)
 
 
+        # ADD WIDGETS TO TOGGLE VISIBILITY OF LAYERS
+        toggle_image = Button(
+            label="Toggle Heatmap")
+        toggle_image.width = 100
+        toggle_image.js_on_click(CustomJS(args=dict(image=image),
+                                          code="image.visible=!image.visible"))
+
+        toggle_scatter = Button(
+            label="Toggle Scatter Plot")
+        toggle_scatter.width = 100
+        toggle_scatter.js_on_click(CustomJS(args=dict(scatter=scatter),
+                                          code="scatter.visible=!scatter.visible"))
+
+
         # SAVE AND SHOW PLOT
         name = file.replace('.mat', '') + '_composite.html'
         title = "Composite Plot: " + file[file.find('/') + 1::]
         output_file(name, title)
 
+
+        # OPEN / SHOW
+        doc_layout = column(
+            [p,
+            row([widgetbox([toggle_image, toggle_scatter], width=10)], height=50, sizing_mode="fixed")],
+            sizing_mode="scale_width")
+
+
         if auto_open:
-            show(p)
+            show(doc_layout)
         else:
-            save(p)
+            save(doc_layout)
 
 
 
