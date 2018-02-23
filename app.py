4af265a14c12ac48c506fcd4b719896d6f0a4ad5
Riley Taylor <darkrilin@gmail.com>
2018-02-23 22:12:40 +1100

    Attempt at fixing sandboxing/security flaw
    Also cleaned up the directories, hopefully uploaded files aren't kept
    around when no longer needed. This should fix any cross-compatibility
    issues with different server configurations.
    
    Hopefully it also stops python from accessing the root directory of the
    server drive, instead accessing flasks root - which may be different on
    different configurations.
    
    Still need to investigate how secure this is with more testing, but it's
    a hell of a lot more secure than it was.

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request, redirect, session
 from werkzeug.utils import secure_filename
 import main
-import os
 
-UPLOAD_FOLDER = 'uploads/'
+from os.path import dirname as os_dir_name, isdir as os_isdir, join as os_path_join
+from os import environ as os_environ, makedirs as os_make_dirs, remove as os_remove
+
+UPLOAD_FOLDER = '/uploads/'
 ALLOWED_EXTENSIONS = {'mat'}
-HEROKU = os.environ.get('HEROKU', 0)
+HEROKU = os_environ.get('HEROKU', 0)
 
-if HEROKU == 0:
-    print("Offline")
-    UPLOAD_FOLDER = "/uploads/"
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
@@ -20,9 +18,9 @@ def allowed_file(filename):
 
 
 def ensure_dir(f):
-    d = os.path.dirname(f)
-    if not os.path.isdir(d):
-        os.makedirs(d)
+    d = os_dir_name(f)
+    if not os_isdir(d):
+        os_make_dirs(d)
 
 
 @app.route('/')
@@ -33,6 +31,8 @@ def show_main():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        session['output_path'] = ""
+
         # check if the post request has the file part
         if 'file' not in request.files:
             print("Redirecting to request URL")
@@ -47,78 +47,69 @@ def upload_file():
 
         # loop through and save all uploaded files
         files_to_process = []
+        upload_path = os_path_join(app.root_path + app.config['UPLOAD_FOLDER'])
+        static_path = os_path_join(app.root_path + '/static/graphs/')
+        ensure_dir(upload_path)
+        ensure_dir(static_path)
 
         for each_file in uploaded_files:
-
             if each_file and allowed_file(each_file.filename):
                 filename = secure_filename(each_file.filename)
-
                 try:
-                    ensure_dir(app.config['UPLOAD_FOLDER'])
-                    each_file.save(app.config['UPLOAD_FOLDER'] + filename)
-                    files_to_process.append(filename)
+                    each_file.save(upload_path + filename)
+                    files_to_process.append(upload_path + filename)
                 except PermissionError:
-                    return redirect('/static/upload_error.html')
+                    return redirect('/upload_error.html')
 
         # now do the processing
         if len(files_to_process) == 0:
             # redirect if none of the files are uploaded correctly
-            return redirect('/static/upload_error.html')
+            return redirect('/upload_error.html')
 
         elif len(files_to_process) == 1:
             # only one file selected, graph normally
             current_file = files_to_process[0]
             try:
-                main.bokeh_composite(app.config['UPLOAD_FOLDER'] + current_file, auto_open=False)
-                session['composite_path'] = '/graph/' + current_file[:-4] + '_composite' + '.html'
-                return redirect('/graph/' + current_file[:-4] + '_composite' + '.html')
+                main.graph_single(current_file, auto_open=False, dir=static_path)
+                session['output_path'] = os_path_join('/graphs/' + current_file.split("/")[-1][:-4] + '.html')
             except KeyError:
-                return redirect('/static/key_error.html')
+                return redirect('/key_error.html')
 
         elif len(files_to_process) > 1:
             # graph all files into same document
+            main.graph_multiple(files_to_process, scatter=True, heatmap=True, auto_open=False)
             print(files_to_process)
-
             # TODO: CREATE FUNCTION IN main.py WHICH HANDLES MULTIPLE GRAPH FILES
 
-        # fail safe
-        return redirect("/")
+        # clean up temp files
+        for file in files_to_process:
+            os_remove(file)
+
+        # redirect to output graph
+        return redirect('/output')
 
 
-@app.route('/composite')
-def composite_selected():
-    return redirect(session['composite_path'])
-
-
-@app.route('/graph/<filename>')
-def graph_file(filename):
-    try:
-        file = open(app.config['UPLOAD_FOLDER'] + filename)
-    except Exception as ex:
-        print(ex)
-        return redirect('/')
-
-    file_html = file.read()
-    file.close()
-    return file_html
+@app.route('/output')
+def output_graph():
+    return redirect(session['output_path'])
 
 
 @app.route('/example')
 def example():
-    return redirect("/static/example.html")
+    return redirect("example.html")
 
 
 def insert_substring(main_string, substring, index):
     return main_string[:index] + substring + main_string[index:]
 
 
-app.secret_key = os.environ.get('SECRET_KEY', None)
+app.secret_key = os_environ.get('SECRET_KEY', None)
 if app.secret_key is None:
     print("Secret key not found. Exiting app.")
     exit()
 else:
     if HEROKU:
-        PORT = int(os.environ.get('PORT', 5000))
+        PORT = int(os_environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=PORT)
     else:
         app.run(host='localhost')
