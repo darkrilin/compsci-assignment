bde95d4129702e8a7d9348257962f5b93a460d86
Riley Taylor <darkrilin@gmail.com>
2018-02-25 09:47:26 +1100

    Generated pages should no longer be cached
    I've reduced the rendered page cache times so that newly created
    documents will display properly, rather than loading a cached document
    previously created with the same data.
    
    This should fix any weird behaviour in a few browsers which weren't
    displaying the updated graphs properly.

--- app.py
+++ app.py
@@ -23,6 +23,17 @@ def ensure_dir(f):
         os_make_dirs(d)
 
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
 @app.route('/')
 def show_main():
     return render_template('website.html')
